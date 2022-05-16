70bedee498243da01f0ca8db6c48ca1fe2797839
obleus <odney@Odneys-Air.attlocal.net>
2022-05-15 22:53:38 -0400

    added server and get/api notes

---
+++ server.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    res.json(parseNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    parseNotes.push(req.body);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
+    res.json("You have successfully added a note!");
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
